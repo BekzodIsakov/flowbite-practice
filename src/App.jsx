ce7676d8148542ec0ad18b50c14cd48f1abb04d4
Bekzod <bekzodisakov18@gmail.com>
2024-08-10 15:57:47 +0500

    remove header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Countries from "./pages/Countries";
-import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
         <Route path='/' element={<Countries />} />
       </Routes>
--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -10,39 +10,11 @@ import {
   TableHeadCell,
   TableRow,
   Button,
-  Flowbite,
 } from "flowbite-react";
 
 export default function Countries() {
   const [countries, setCountries] = useState([]);
   const [selectedCountries, setSelectedCountries] = useState([]);
-
-  const customTheme = {
-    root: {
-      base: "w-full text-left text-sm dark:text-gray-400",
-      shadow:
-        "absolute left-0 top-0 -z-10 h-full w-full rounded-lg bg-white drop-shadow-md dark:bg-black",
-      wrapper: "relative",
-    },
-    body: {
-      base: "group/body",
-      cell: {
-        base: "px-6 py-4 group-first/body:group-first/row:first:rounded-tl-lg group-first/body:group-first/row:last:rounded-tr-lg group-last/body:group-last/row:first:rounded-bl-lg group-last/body:group-last/row:last:rounded-br-lg",
-      },
-    },
-    head: {
-      base: "group/head text-xs p-8 uppercase dark:text-gray-400 bg-orange-400",
-      cell: {
-        base: "border-none p-16 bg-blue-500 group-first/head:first:rounded-tl-lg group-first/head:last:rounded-tr-lg dark:bg-gray-700",
-      },
-    },
-    row: {
-      base: "group/row",
-      hovered: "hover:bg-gray-50 dark:hover:bg-gray-600",
-      striped:
-        "odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700",
-    },
-  };
 
   useEffect(() => {
     async function fetchCountries() {
@@ -72,8 +44,8 @@ export default function Countries() {
       <HeroSection carouselElements={countries.slice(0, 8)} />
 
       <div className='max-w-[1140px] mx-auto mt-4'>
-        <Table striped theme={customTheme}>
-          <TableHead className='bg-slate-400'>
+        <Table striped>
+          <TableHead>
             <TableHeadCell>Name</TableHeadCell>
             <TableHeadCell>Population</TableHeadCell>
             <TableHeadCell>Capital</TableHeadCell>
@@ -93,11 +65,8 @@ export default function Countries() {
                 <TableCell>{c.population}</TableCell>
                 <TableCell>{c.capital}</TableCell>
                 <TableCell>
-                  <Button
-                    onClick={() => selectUnselectCountry(c)}
-                    className='bg-transparent enabled:hover:bg-transparent focus:ring-0 w-7 h-6 p-0 focus-visible:bg-transparent focus:bg-transparent'
-                  >
-                    <BsEye color={"black"} />
+                  <Button onClick={() => selectUnselectCountry(c)}>
+                    Tanlash
                   </Button>
                 </TableCell>
               </TableRow>
